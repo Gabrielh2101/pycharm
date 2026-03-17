e761833668344ae6003ac01167c9330e3a6b7780
labsfiap <gabrielhs2101@gmail.com>
2026-03-16 22:35:55 -0300

    Aula 3

---
+++ TERCEIRA AULA.py
@@ -0,0 +1,6 @@
+
+media_final = float(input("Média Final: "))
+if media_final >= 6:
+    print("Aprovado")
+else:
+    print("Reprovado")
---
+++ exercicios aula3.py
@@ -0,0 +1,51 @@
+'''
+velocidade = int(input("Insira a velocida do carro: "))
+if velocidade >= 80:
+    #multa = (velocidade - 80) * 5
+    #print("Carro pagará: R$", multa,"")
+else:
+    print("Carro na velocidade permitida")
+'''
+'''
+num1 = int(input("Insira o seu primeiro número: "))
+num2 = int(input("Insira o seu segundo número: "))
+num3 = int(input("Insira o seu terceiro número: "))
+maior = num1
+if num2 >= num3 and num2 >= num1:
+    maior = num2
+if num3 >=
+if num1 >= num2 and num1 >= num3:
+    if num2 >= num3:
+        #print("Os números em forma crescente: ",num1," - ",num2," - ",num3)
+    else:
+        print("Os números em forma crescente: ",num1," - ",num3," - ",num2)
+elif num2 >= num1 and num2 >= num3:
+    if num1 >= num3:
+        print("Os números em forma crescente: ",num2," - ",num1," - ",num3)
+    else:
+        print("Os números em forma crescente: ",num2," - ",num3," - ",num1)
+else:
+    if num1 >= num2:
+        print("Os números em forma crescente: ",num3," - ",num1," - ",num2)
+    else:
+        print("Os números em forma crescente: ",num3," - ",num2," - ",num1)
+'''
+'''
+sal = float(input("Insira seu salário: "))
+if sal >= 1250.00:
+    sal_new = (sal * 0.10) + sal
+    print("Você receberá um aumeno salárial de R$", sal ," . Parabéns pelo seu aumento.")
+else:
+    sal_new = (sal * 0.15) + sal
+    print("Você receberá um aumeno salárial de R$", (sal * 0.15) + sal , " . Parabéns pelo seu aumento.")
+'''
+
+'''
+dis = float(input("Qual foi a distância percorrida em km:"))
+if dis >= 200:
+    passa = dis * 0.45
+    print("Você pagará R$",passa)
+else:
+    passa = dis * 0.50
+    print("Você pagará R$", passa)
+'''
